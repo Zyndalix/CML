7381fae1de76b340f194c3f763076f142d1ce080
BCC <Bram.Clarenbeek@gmail.com>
2024-02-27 19:28:35 +0100

    Started working on the graphing part, worked on overall readability, added JFreeChart library

--- Main.java
+++ Main.java
@@ -5,6 +5,11 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+//UI
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import ui.*;
+
 
 class Main {
 
@@ -26,11 +31,35 @@ class Main {
 	}
 
 	public static void main(String[] args) {
+		setUpUI();
 
+		//Interpreting data in main right now, needs to change to button in UI
+		ArrayList<ArrayList<String>> chartData = interpretData();
+
+	}
+
+	private static ArrayList<ArrayList<String>> interpretData(){
 		String variables = getContentsOfFile("variables");
 		String rules = getContentsOfFile("rules");
+
+		//Check UI for iterations, 10 is default for now
 		int iterations = 10;
 
-		ArrayList<ArrayList<String>> chartData = Interpreter.main(variables, rules, iterations);
+		return Interpreter.main(variables, rules, iterations);
+	}
+
+	private static void setUpUI(){
+		//Still need to set up UI in this method
+
+		// following two lines are not important
+		String s = UIManager.getSystemLookAndFeelClassName();
+		System.out.println(s);
+
+		// boilerplate code needed for setting up the window, don't delete
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrameWindow window = new JFrameWindow();
+			}
+		});
 	}
 }
---
+++ graphing/CreateGraph.java
@@ -0,0 +1,15 @@
+package graphing;
+
+import org.jfree.chart.*;
+
+import java.util.ArrayList;
+
+public class CreateGraph {
+    public static void main(String[] args) {
+        System.out.print("Test");
+        //ArrayList<ArrayList<String>> chartData
+        //This file is only for testing the graph program for now
+
+        //Should eventually output jpeg for UI class to use
+    }
+}
