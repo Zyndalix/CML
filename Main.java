f38259ee67390ecd2eab4109aab6c284affcb9dc
fireFerry <info@fireferry.xyz>
2024-03-27 01:29:40 +0100

    Added artifact (.jar) configuration and extracts files if not found.

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ import interpreter.*;
 import graphing.*;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -12,12 +11,35 @@ import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import org.jfree.chart.JFreeChart;
+import org.xml.sax.ErrorHandler;
 import ui.*;
 
 
 public class Main {
 
 	public static void main(String[] args) {
+		extractFiles();
 		CreateGraph.setUpUI();
+	}
+	private static void extractFiles() {
+		String[] filesToExtract	= {"rules", "variables", "iterations"};
+
+		for (String fileName: filesToExtract) {
+			File file = new File(fileName);
+			if (!file.exists()) {
+				try (InputStream inputStream = Main.class.getResourceAsStream(fileName);
+					 OutputStream outputStream = new FileOutputStream(file)) {
+					byte[] buffer = new byte[1024];
+					int length;
+					while (true) {
+                        assert inputStream != null;
+                        if (!((length = inputStream.read(buffer)) > 0)) break;
+                        outputStream.write(buffer, 0, length);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
